b06eb28a53ffc39f35f298e0194adb3a49d0367d
Humberto BM Junior <64866505+jrhumberto@users.noreply.github.com>
2021-10-03 18:44:20 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,29 @@
 var request = new XMLHttpRequest();
 var data_url = 'data/data.json';
 
+
+var i = 0;
+// var A = 0;
+var tag = document.getElementById("text");
+var html = document.getElementById("text").innerHTML;
+var attr = tag.setAttribute("data", html);
+var txt = tag.getAttribute("data");
+var speed = 170;
+
+function typeWriter() {
+    if (i <= txt.length) {
+        document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+        //console.log(document.getElementById("text").innerHTML);
+ }
+
+typeWriter();
+
+
+
+
 request.open('GET', data_url);
 
 function build_html(hj, message,deaths, state, city, confirmed, confirmed_diff, date){
